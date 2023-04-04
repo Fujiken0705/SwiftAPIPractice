fe607186b9259c38e2a25020f119901806300888
Fujiken0705 <kentofujita.new@gmail.com>
2023-04-04 22:31:40 +0900

    TableView実装

--- SwiftAPIPractice/ViewController.swift
+++ SwiftAPIPractice/ViewController.swift
@@ -9,15 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+@IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidLoad()
     }
 
-
 }
 
+extension ViewController: UITableViewDataSource {
+
+    // セクションの個数を返す
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    // セクションごとにセルの個数を返す
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    // セルの中身を返す
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: TableViewCell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! TableViewCell
+        return cell
+    }
+
+    // セルの高さを返す
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+
+    // セルがタップされたときの処理を書く
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        return
+    }
+
+    // スクロールしたときの処理を書く
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        return
+    }
+}
